dad452a5c58e8954aff1063dc5d1a7f42c305090
ThLourenco <thilourenco01@gmail.com>
2024-10-15 23:29:39 -0300

    Criei a tela de login

--- app/src/main/java/com/example/applistatelefonica/SplashActivity.kt
+++ app/src/main/java/com/example/applistatelefonica/SplashActivity.kt
@@ -1,21 +1,31 @@
 package com.example.applistatelefonica
+
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this, LoginActivity::class.java))
+            }, 2000)
+
             insets
-
-
         }
+
     }
 }
